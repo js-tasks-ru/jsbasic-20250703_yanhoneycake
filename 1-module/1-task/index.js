6c3f484f3764fa149f06e819a8651d75ff4cebc0
Yan Pavlov <honeycakeend@gmail.com>
2025-07-06 10:11:32 +0300

    Tasks for "Занятие 1: Основы JavaScript"

---
+++ 1-module/1-task/index.js
@@ -0,0 +1,8 @@
+function factorial(n) {
+  let result = n > 1 ? n : 1; 
+  while(n > 1){
+    result *= n - 1;
+    n--;
+  }
+  return result;
+}
---
+++ 1-module/2-task/index.js
@@ -0,0 +1,24 @@
+/**
+ * Эту функцию трогать не нужно
+ */
+function print(text) {
+  console.log(text);
+}
+
+/**
+ * Эту функцию нужно поменять так,
+ * чтобы функция sayHello работала корректно
+ */
+function isValid(name) {
+  return name !== null && !name.includes(" ") && name.length > 3 ? true : false;
+}
+
+function sayHello() {
+  let userName = prompt('Введите ваше имя');
+
+  if (isValid(userName)) {
+    print(`Welcome back, ${userName}!`);
+  } else {
+    print('Некорректное имя');
+  }
+}
