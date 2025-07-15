99cbe0a2bc7e08ff9f0094726bbdbd51ef0bdda7
Yan Pavlov <honeycakeend@gmail.com>
2025-07-15 23:36:45 +0300

    third module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,5 @@
 function camelize(str) {
-  // ваш код...
+    return str.split('-').map((elem, index) => 
+        index === 0 ? elem : elem.charAt(0).toUpperCase() + elem.slice(1))
+        .join('');
 }
