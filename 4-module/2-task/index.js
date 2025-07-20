eb98524dbf31dd1800fcaf58dbb69619e091ad38
Yan Pavlov <honeycakeend@gmail.com>
2025-07-20 20:40:34 +0300

    module-4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,8 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+    const rows = table.rows;
+
+    for (let i = 0; i < rows.length; i++) {
+        const cell = rows[i].cells[i];
+        cell.style.backgroundColor = 'red';
+    }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,32 @@
 function highlight(table) {
-  // ваш код...
+    const rows = table.tBodies[0].rows;
+    for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        const cells = row.cells;
+        
+        const statusCell = cells[3];
+        const genderCell = cells[2];
+        const ageCell = cells[1];
+
+        const available = statusCell.dataset.available;
+        if (available === 'true') {
+            row.classList.add('available');
+        } else if (available === 'false') {
+            row.classList.add('unavailable');
+        } else {
+            row.hidden = true;
+        }
+        
+        const gender = genderCell.textContent;
+        if (gender === 'm') {
+            row.classList.add('male');
+        } else if (gender === 'f') {
+            row.classList.add('female');
+        }
+        
+        const age = parseInt(ageCell.textContent, 10);
+        if (!isNaN(age) && age < 18) {
+            row.style.textDecoration = 'line-through';
+        }
+    }
 }
